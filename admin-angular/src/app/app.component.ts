6d88712d284b7ff0bfc709dc89d83e059c1857c4
JTACCAG <jtaccag@gmail.com>
2022-09-30 20:42:17 -0500

    connect api and admin

--- admin-angular/src/app/app.component.ts
+++ admin-angular/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AppService } from './services/app.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'admin-angular';
+export class AppComponent implements OnInit {
+  title: any;
+
+  constructor(
+    private appService: AppService
+  ){ }
+
+  ngOnInit(): void {
+    console.log('entro');
+    this.appService.getHello().subscribe(resp => {
+      console.log(resp);
+      this.title = resp;
+    }, err => {
+      console.log('ERROR');
+      console.log(err);
+    })
+    
+  }
 }
